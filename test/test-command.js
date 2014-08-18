b85ead3428107d44928eeaf7a728cadccfbd8dd1
Trevor Livingston <trlivingston@paypal.com>
2014-08-18 08:30:24 -0500

    updated generator to call testable file

--- test/test-command.js
+++ test/test-command.js
@@ -1,13 +1,12 @@
 'use strict';
 
 var test = require('tape'),
-    spawn = require('child_process').execFile,
+    swaggerize = require('../bin/lib/swaggerize'),
     fs = require('fs'),
     path = require('path'),
     mkdirp = require('mkdirp');
 
 test('swaggerize command', function (t) {
-    var commandPath = path.resolve('bin/swaggerize.js');
 
     mkdirp.sync(path.resolve('test/temp'));
 
@@ -31,78 +30,64 @@ test('swaggerize command', function (t) {
     t.test('no handlers or models', function (t) {
         t.plan(1);
 
-        console.log(commandPath);
+        var code = swaggerize(['foo', 'bar', '--api', 'test/fixtures/api.json']);
 
-        var cmd = spawn(commandPath, ['--api', 'test/fixtures/api.json']);
-
-        cmd.on('close', function (code) {
-            t.strictEqual(code, 1, 'error code 1.');
-        });
+        t.strictEqual(code, 1, 'error code 1.');
     });
 
     t.test('tests but no handlers and models', function (t) {
         t.plan(1);
 
-        var cmd = spawn(commandPath, ['--api', 'test/fixtures/api.json', '--tests', 'test/temp/tests']);
+        var code = swaggerize(['foo', 'bar', '--api', 'test/fixtures/api.json', '--tests', 'test/temp/tests']);
 
-        cmd.on('close', function (code) {
-            t.strictEqual(code, 1, 'error code 1.');
-        });
+        t.strictEqual(code, 1, 'error code 1.');
     });
 
     t.test('invalid schema fails', function (t) {
         t.plan(1);
 
-        var cmd = spawn(commandPath, ['--api', 'test/fixtures/badapi.json', '--handlers', 'test/temp/handlers']);
+        var code = swaggerize(['foo', 'bar', '--api', 'test/fixtures/badapi.json', '--handlers', 'test/temp/handlers']);
 
-        cmd.on('close', function (code) {
-            t.strictEqual(code, 1, 'error code 1.');
-        });
+        t.strictEqual(code, 1, 'error code 1.');
     });
 
     t.test('handlers', function (t) {
         t.plan(9);
 
-        var cmd = spawn(commandPath, ['--api', 'test/fixtures/api.json', '--handlers', 'test/temp/handlers']);
+        var code = swaggerize(['foo', 'bar', '--api', 'test/fixtures/api.json', '--handlers', 'test/temp/handlers']);
 
-        cmd.on('close', function (code) {
-            t.ok(!code, 'no error code.');
-            t.ok(fs.existsSync(path.resolve('test/temp/handlers')), 'handlers dir exists');
-            t.ok(fs.existsSync(path.resolve('test/temp/handlers/goodbye')), 'goodbye dir exists');
-            t.ok(fs.existsSync(path.resolve('test/temp/handlers/goodbye/{subject}.js')), 'goodbye/{subject}.js exists');
-            t.ok(fs.existsSync(path.resolve('test/temp/handlers/hello/{subject}.js')), 'hello/{subject}.js exists');
-            t.ok(fs.existsSync(path.resolve('test/temp/handlers/sub')), 'sub dir exists');
-            t.ok(fs.existsSync(path.resolve('test/temp/handlers/sub/{id}.js')), 'sub/{id}.js exists');
-            t.ok(fs.existsSync(path.resolve('test/temp/handlers/sub/{id}')), 'sub/{id} dir exists');
-            t.ok(fs.existsSync(path.resolve('test/temp/handlers/sub/{id}/path.js')), 'sub/{id}/path.js exists');
-        });
+        t.ok(!code, 'no error code.');
+        t.ok(fs.existsSync(path.resolve('test/temp/handlers')), 'handlers dir exists');
+        t.ok(fs.existsSync(path.resolve('test/temp/handlers/goodbye')), 'goodbye dir exists');
+        t.ok(fs.existsSync(path.resolve('test/temp/handlers/goodbye/{subject}.js')), 'goodbye/{subject}.js exists');
+        t.ok(fs.existsSync(path.resolve('test/temp/handlers/hello/{subject}.js')), 'hello/{subject}.js exists');
+        t.ok(fs.existsSync(path.resolve('test/temp/handlers/sub')), 'sub dir exists');
+        t.ok(fs.existsSync(path.resolve('test/temp/handlers/sub/{id}.js')), 'sub/{id}.js exists');
+        t.ok(fs.existsSync(path.resolve('test/temp/handlers/sub/{id}')), 'sub/{id} dir exists');
+        t.ok(fs.existsSync(path.resolve('test/temp/handlers/sub/{id}/path.js')), 'sub/{id}/path.js exists');
     });
 
     t.test('models', function (t) {
         t.plan(3);
 
-        var cmd = spawn(commandPath, ['--api', 'test/fixtures/api.json', '--models', 'test/temp/models']);
+        var code = swaggerize(['foo', 'bar', '--api', 'test/fixtures/api.json', '--models', 'test/temp/models']);
 
-        cmd.on('close', function (code) {
-            t.ok(!code, 'no error code.');
-            t.ok(fs.existsSync(path.resolve('test/temp/models')), 'models dir exists');
-            t.ok(fs.existsSync(path.resolve('test/temp/models/user.js')), 'user.js exists');
-        });
+        t.ok(!code, 'no error code.');
+        t.ok(fs.existsSync(path.resolve('test/temp/models')), 'models dir exists');
+        t.ok(fs.existsSync(path.resolve('test/temp/models/user.js')), 'user.js exists');
     });
 
     t.test('tests', function (t) {
         t.plan(6);
 
-        var cmd = spawn(commandPath, ['--api', 'test/fixtures/api.json', '--handlers', 'test/temp/handlers', '--models', 'test/temp/models', '--tests', 'test/temp/tests']);
+        var code = swaggerize(['foo', 'bar', '--api', 'test/fixtures/api.json', '--handlers', 'test/temp/handlers', '--models', 'test/temp/models', '--tests', 'test/temp/tests']);
 
-        cmd.on('close', function (code) {
-            t.ok(!code, 'no error code.');
-            t.ok(fs.existsSync(path.resolve('test/temp/tests')), 'tests dir exists');
-            t.ok(fs.existsSync(path.resolve('test/temp/tests/test_goodbye_{subject}.js')), 'test_goodbye_{subject}.js exists');
-            t.ok(fs.existsSync(path.resolve('test/temp/tests/test_hello_{subject}.js')), 'test_hello_{subject}.js exists');
-            t.ok(fs.existsSync(path.resolve('test/temp/tests/test_sub_{id}.js')), 'test_sub_{id}.js exists');
-            t.ok(fs.existsSync(path.resolve('test/temp/tests/test_sub_{id}_path.js')), 'test_sub_{id}_path.js exists');
-        });
+        t.ok(!code, 'no error code.');
+        t.ok(fs.existsSync(path.resolve('test/temp/tests')), 'tests dir exists');
+        t.ok(fs.existsSync(path.resolve('test/temp/tests/test_goodbye_{subject}.js')), 'test_goodbye_{subject}.js exists');
+        t.ok(fs.existsSync(path.resolve('test/temp/tests/test_hello_{subject}.js')), 'test_hello_{subject}.js exists');
+        t.ok(fs.existsSync(path.resolve('test/temp/tests/test_sub_{id}.js')), 'test_sub_{id}.js exists');
+        t.ok(fs.existsSync(path.resolve('test/temp/tests/test_sub_{id}_path.js')), 'test_sub_{id}_path.js exists');
     });
 
 });
