bdd1f2fe3c5715b184c52d05f18827a8f4c1978e
Trevor Livingston <trlivingston@paypal.com>
2014-08-21 14:21:16 -0500

    input validation tests

--- test/test-validation.js
+++ test/test-validation.js
@@ -4,7 +4,9 @@ var test = require('tape'),
     validation = require('../lib/validation');
 
 test('validation', function (t) {
-    var validator = validation.output({
+    var outputvalid, inputvalid;
+
+    outputvalid = validation.output({
         "id": "User",
         "required": ["id", "name"],
         "properties": {
@@ -17,25 +19,61 @@ test('validation', function (t) {
         }
     });
 
+    inputvalid = validation.input({
+        paramType: 'query',
+        name: 'id',
+        type: 'integer',
+        required: true
+    }, 'string');
+
+    t.test('input pass', function (t) {
+        t.plan(1);
+
+        inputvalid({
+            param: function () {
+                return this.params.id;
+            },
+            params: {
+                id: 1
+            },
+        }, {}, function (error) {
+            t.ok(!error, 'no error.');
+        });
+    });
+
+    t.test('input fail (not present)', function (t) {
+        t.plan(1);
+
+        inputvalid({
+            param: function () {
+                return undefined;
+            },
+            params: {
+            },
+        }, {}, function (error) {
+            t.ok(error, 'error.');
+        });
+    });
+
     t.test('output pass', function (t) {
         t.plan(1);
 
-        validator({
+        outputvalid({
             id: 1,
             name: 'Joe'
         }, function (error) {
-            t.ok(!error);
+            t.ok(!error, 'no error.');
         });
     });
 
     t.test('output fail', function (t) {
         t.plan(1);
 
-        validator({
+        outputvalid({
             id: 'a',
             name: 'Joe'
         }, function (error) {
-            t.ok(error);
+            t.ok(error, 'error.');
         });
     });
 
