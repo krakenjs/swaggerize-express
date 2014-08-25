4ec2873e83adfb38cdea9d6c42c07d5651f25f78
Trevor <tlivings@gmail.com>
2014-08-24 19:47:30 -0500

    removed unnecessary defaulting of undefined parameters and provide better primitive validation.

--- test/test-validation.js
+++ test/test-validation.js
@@ -54,6 +54,103 @@ test('validation', function (t) {
         });
     });
 
+    t.test('input validation skip (not present, not required)', function (t) {
+        t.plan(1);
+
+        validation.input({
+            paramType: 'query',
+            name: 'id',
+            required: false
+        }, 'integer')({
+            param: function () {
+                return undefined;
+            },
+            params: {
+            },
+        }, {}, function (error) {
+            t.ok(!error, 'no error.');
+        });
+    });
+
+    t.test('input coerce to float (pass)', function (t) {
+        t.plan(1);
+
+        validation.input({
+            paramType: 'query',
+            name: 'id',
+            required: true
+        }, 'float')({
+            param: function () {
+                return this.params.id;
+            },
+            params: {
+                id: '1.0'
+            },
+        }, {}, function (error) {
+            error && console.error(error);
+            t.ok(!error, 'no error.');
+        });
+    });
+
+    t.test('input coerce to byte (pass)', function (t) {
+        t.plan(1);
+
+        validation.input({
+            paramType: 'query',
+            name: 'id',
+            required: true
+        }, 'byte')({
+            param: function () {
+                return this.params.id;
+            },
+            params: {
+                id: 'a'
+            },
+        }, {}, function (error) {
+            error && console.error(error);
+            t.ok(!error, 'no error.');
+        });
+    });
+
+    t.test('input coerce to boolean (pass)', function (t) {
+        t.plan(1);
+
+        validation.input({
+            paramType: 'query',
+            name: 'id',
+            required: true
+        }, 'boolean')({
+            param: function () {
+                return this.params.id;
+            },
+            params: {
+                id: 1
+            },
+        }, {}, function (error) {
+            error && console.error(error);
+            t.ok(!error, 'no error.');
+        });
+    });
+
+    t.test('input coerce to string (pass)', function (t) {
+        t.plan(1);
+
+        validation.input({
+            paramType: 'query',
+            name: 'id',
+            required: true
+        }, 'string')({
+            param: function () {
+                return this.params.id;
+            },
+            params: {
+                id: 1
+            },
+        }, {}, function (error) {
+            t.ok(!error, 'no error.');
+        });
+    });
+
     t.test('input fail (wrong type)', function (t) {
         t.plan(1);
 
