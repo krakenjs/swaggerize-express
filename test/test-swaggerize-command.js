d0e464770fcca26c5985505946a3a043c67addd1
Trevor Livingston <trlivingston@paypal.com>
2014-09-03 14:56:36 -0500

    copy over fixtures so as to not have git diffs

--- test/test-swaggerize-command.js
+++ test/test-swaggerize-command.js
@@ -7,50 +7,58 @@ var test = require('tape'),
     mkdirp = require('mkdirp');
 
 test('swaggerize command', function (t) {
-    var cwd = process.cwd();
+    var cwd, writeStream;
+
+    cwd = process.cwd();
 
     mkdirp.sync(path.resolve('test/temp'));
 
-    process.chdir(path.resolve('test/fixtures'));
+    process.chdir(path.resolve('test/temp'));
 
-    t.on('end', function () {
-        process.chdir(cwd);
+    writeStream = fs.createWriteStream('package.json');
 
-        function rm(dir) {
-            var files = fs.readdirSync(dir);
+    writeStream.on('finish', function () {
 
-            files && files.forEach(function (file) {
-                var info = fs.statSync(file = path.join(dir, file));
+        t.on('end', function () {
+            process.chdir(cwd);
 
-                info.isFile() && fs.unlinkSync(file);
-                info.isDirectory() && rm(file);
+            function rm(dir) {
+                var files = fs.readdirSync(dir);
+
+                files && files.forEach(function (file) {
+                    var info = fs.statSync(file = path.join(dir, file));
+
+                    info.isFile() && fs.unlinkSync(file);
+                    info.isDirectory() && rm(file);
+                });
+
+                fs.rmdirSync(dir);
+            }
+
+            rm(path.resolve('test/temp'));
+        });
+
+        t.test('npm devDependencies', function (t) {
+            t.plan(5);
+
+            execFile('../../bin/swaggerize.js', ['--api', '../fixtures/api.json', '--handlers', 'handlers', '--models', 'models', '--tests', 'tests'], function (error, stdout, stderr) {
+                var pkg;
+
+                stdout && console.log(stdout.toString());
+                stderr && console.log(stderr.toString());
+                error && console.error(error);
+
+                t.ok(!error, 'no error.');
+
+                pkg = require(path.join(__dirname, 'temp/package.json'));
+
+                t.ok(pkg.devDependencies, 'devDependencies exists.');
+                t.ok(pkg.devDependencies.tape, 'tape exists.');
+                t.ok(pkg.devDependencies['body-parser'], 'body-parser exists.');
+                t.ok(pkg.devDependencies.supertest, 'supertest exists.');
             });
-
-            fs.rmdirSync(dir);
-        }
-
-        rm(path.resolve('test/temp'));
-    });
-
-    t.test('npm devDependencies', function (t) {
-        t.plan(5);
-
-        execFile('../../bin/swaggerize.js', ['--api', 'api.json', '--handlers', '../temp/handlers', '--models', '../temp/models', '--tests', '../temp/tests'], function (error, stdout, stderr) {
-            var pkg;
-
-            stdout && console.log(stdout.toString());
-            stderr && console.log(stderr.toString());
-            error && console.error(error);
-
-            t.ok(!error, 'no error.');
-
-            pkg = require(path.join(__dirname, 'fixtures/package.json'));
-
-            t.ok(pkg.devDependencies, 'devDependencies exists.');
-            t.ok(pkg.devDependencies.tape, 'tape exists.');
-            t.ok(pkg.devDependencies['body-parser'], 'body-parser exists.');
-            t.ok(pkg.devDependencies.supertest, 'supertest exists.');
         });
     });
 
+    fs.createReadStream('../fixtures/package.json').pipe(writeStream);
 });
