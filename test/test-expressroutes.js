4f9cab55f229e9a562743d8d4050555ee4759eb0
nbcraft <nbcraft@users.noreply.github.com>
2016-04-21 05:52:13 +0200

    Routes : Added HTTP 405 handler middleware
    * Routes : Added HTTP 405 handler middleware
    
    * Routes : 405 Handler - Moved new middleware to last in stack
    
    * Lint : Removed unnecessary semicolon
    
    * Module code style compliance

--- test/test-expressroutes.js
+++ test/test-expressroutes.js
@@ -7,7 +7,7 @@ var test = require('tape'),
 test('express routes', function (t) {
 
     t.test('test api', function (t) {
-        t.plan(3);
+        t.plan(4);
 
         var app = express(), child = express();
 
@@ -28,9 +28,10 @@ test('express routes', function (t) {
 
             stack = Array.prototype.slice.call(parent._router.stack, 3);
 
-            t.strictEqual(stack.length, 2, '2 routes added.');
+            t.strictEqual(stack.length, 3, '3 routes added.');
             t.strictEqual(stack[0].route.path, '/v1/petstore/api-docs', 'api-docs added.');
             t.strictEqual(stack[1].route.path, '/v1/petstore/pets/:id', 'hello added.');
+            t.strictEqual(stack[2].regexp.toString(), '/^\\/v1\\/petstore\\/pets\\/([^\\/]+?)(?:\\/(?=$))?$/i', 'bleb');
         });
 
         app.use(child);
@@ -60,7 +61,7 @@ test('express routes', function (t) {
     });
 
     t.test('test middlewares in handler', function (t) {
-        t.plan(4);
+        t.plan(5);
 
         var app = express(), child = express();
 
@@ -84,10 +85,11 @@ test('express routes', function (t) {
 
             stack = Array.prototype.slice.call(parent._router.stack, 3);
 
-            t.strictEqual(stack.length, 2, '2 routes added.');
+            t.strictEqual(stack.length, 3, '3 routes added.');
             t.strictEqual(stack[1].route.path, '/v1/petstore/pets', '/pets added.');
             t.strictEqual(stack[1].route.stack.length, 2, '/pets has middleware.');
             t.strictEqual(stack[1].route.stack[0].name, 'm1', '/pets has middleware named m1.');
+            t.strictEqual(stack[2].regexp.toString(), '/^\\/v1\\/petstore\\/pets(?:\\/(?=$))?$/i', 'bleb');
         });
 
         app.use(child);
